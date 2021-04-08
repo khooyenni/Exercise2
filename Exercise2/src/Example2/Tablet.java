ee684e516b6f010327ad2347e2bbaf0276c4c463
khooyenni <73241253+khooyenni@users.noreply.github.com>
2021-04-08 13:17:55 +0800

    Update and rename MicrowaveOven.java to Tablet.java

---
+++ Exercise2/src/Example2/Tablet.java
@@ -0,0 +1,20 @@
+package Example2;
+
+public class Tablet {
+	String brand = "Huawei";
+	String model = "MatePad Pro";
+	String colour = "GRAY";
+	
+	void call(){
+		System.out.println("Tablet can make a call.");
+	}
+	
+	void youtube(){
+		System.out.println("Tablet can watch youtube.");
+	}
+	
+	void notification(){
+		System.out.println("Tablet can receive notification.");
+	}
+
+}
